60276784ce15b3f8a5776b8cd6449fc8b29c72a0
Michael Nutt <michael@nuttnet.net>
2017-12-08 19:00:57 -0500

    add test stubs

--- test/tests.js
+++ test/tests.js
@@ -18,3 +18,93 @@ QUnit.test('it instantiates a studio app', function(assert) {
   const app = new StudioApp();
   assert.equal(app.tags.length, 11);
 });
+
+// TODO:
+
+QUnit.test('.param', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.param with required value missing', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.param with required value present', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.param with default value', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.param with default value replacing empty string', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('constructor setting tags', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('constructor setting options', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.error', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.replaceTokens when tag has valid tokens', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.replaceTokens when tag has no tokens', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.replaceTokens when no values match token', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.replaceTokens when tag is missing', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.showFallbackText with tags', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.showFallbackText with a single tag', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.showFallbackText with a tag with no fallback text', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.resizeTag with text that already fits', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.resizeTag with text that can be resized down to fit', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.resizeTag with text that will never fit', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.resizeTag with missing element', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.overflowRatio', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.waitForImageAssets with background images', function(assert) {
+  assert.expect(0);
+});
+
+QUnit.test('.waitForImageAssets with img tags', function(assert) {
+  assert.expect(0);
+});
